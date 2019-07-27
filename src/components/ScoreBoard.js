716197bcc5fb3b914183c514b6f34b2b60410ba7
Ruairidh Holwill <roore@hotmail.co.uk>
2019-07-27 16:42:42 +0100

    added div.

--- src/components/ScoreBoard.js
+++ src/components/ScoreBoard.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
 
 function ScoreBoard () {
-    
+    return (
+        <div>
+            
+            <h3>P1</h3>
+            <h3>P2</h3>
+        </div>
+    )
 }
 
 export default ScoreBoard;
