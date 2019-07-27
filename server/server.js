a4a4132911de242d2128467c605ccaef7bdebf91
Michael Forbes <michaelforbez@hotmail.co.uk>
2019-07-27 17:08:11 +0100

    package.json updated

--- server/server.js
+++ server/server.js
@@ -22,6 +22,6 @@ MongoClient.connect('mongodb://localhost:27017')
   })
   .catch(console.error);
 
-  app.listen(3000, function() {
+  app.listen(3001, function() {
     console.log(`Listening on port ${this.address().port}`);
   });
