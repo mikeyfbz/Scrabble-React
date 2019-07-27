f1011e49671ea15063f9797ea2047c4209b63e76
Michael Forbes <michaelforbez@hotmail.co.uk>
2019-07-27 16:36:40 +0100

    Tile rack drawn up

--- src/components/TileRack.js
+++ src/components/TileRack.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
+import './TileRack.css';
 
 function TileRack () {
+
+    const letters = ['a', 'e', 'w', 'y', 'h', 'p', 'i'];
+
+    const lettersArray = letters.map((letter, index) => {
+        return <h2 className="tileracktile" key={index}>{letter}</h2>
+    })
     
     return (
-        <div></div>
+        
+        <div className='tilerack'>{lettersArray}</div>
     )
 
 }
--- src/container/Scrabble.js
+++ src/container/Scrabble.js
@@ -1,16 +1,23 @@
 import React, { Component, Fragment } from 'react';
 import Board from '../components/Board';
+import TileRack from '../components/TileRack';
 
 class Scrabble extends Component {
-    super() {
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            
+        }
     }
+    
 
     render() {
 
         return (
             <Fragment>
+                <h1>Scrabble-ish</h1>
                 <Board />
+                <TileRack />
             </Fragment>
         )
     }
