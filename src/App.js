a9f0ad9b86ebb738015ede7f5da3a254f06761ec
Michael Forbes <michaelforbez@hotmail.co.uk>
2019-07-27 15:43:22 +0100

    Scrabble.js added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './App.css';
+import Scrabble from './container/Scrabble';
 
 function App() {
   return (
-    <div>Hi</div>
+    <Scrabble />
   );
 }
 
---
+++ src/container/Scrabble.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Scrabble extends Component {
+    super() {
+
+    }
+
+    render() {
+
+        return (
+            <h1>Hi</h1>
+        )
+    }
+
+
+}
+
+export default Scrabble;
